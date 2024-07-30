952f5bab3599f2bb4ab273985e036834af78ec53
Cristian Ramirez <CristianR@solin.com.co>
2024-07-30 09:39:28 -0500

    Create Model for FibonacciModel

---
+++ src/main/java/com/proteccion/apirest_proteccion/model/FibonacciModel.java
@@ -0,0 +1,48 @@
+package com.proteccion.apirest_proteccion.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "fibonacci")
+public class FibonacciModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String HoraGeneracion;
+    private String SerieCalcula;
+    private String MensajePersonalizado;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHoraGeneracion() {
+        return HoraGeneracion;
+    }
+
+    public void setHoraGeneracion(String horaGeneracion) {
+        HoraGeneracion = horaGeneracion;
+    }
+
+    public String getSerieCalcula() {
+        return SerieCalcula;
+    }
+
+    public void setSerieCalcula(String serieCalcula) {
+        SerieCalcula = serieCalcula;
+    }
+
+    public String getMensajePersonalizado() {
+        return MensajePersonalizado;
+    }
+
+    public void setMensajePersonalizado(String mensajePersonalizado) {
+        MensajePersonalizado = mensajePersonalizado;
+    }
+}
